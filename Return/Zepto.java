17ff56cbd38505e48f3b7bd6d4c8720259685a4c
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 10:49:47 +0530

    Update Zepto.java

--- Return/Zepto.java
+++ Return/Zepto.java
@@ -2,94 +2,93 @@ class Zepto {
 		
     public static void order(String item) {
 		
-		
+	String Price=null;
         if (item=="Milk")
-			System.out.println("Price: ₹50");
+			"Price: 50";
 		
         else if (item=="Bread") 
-			System.out.println("Price: ₹30");
+			"Price: 30";
 		
         else if (item=="Eggs")
-			System.out.println("Price: ₹60");
+			"Price: 60";
 		
         else if (item=="Butter") 
-			System.out.println("Price: ₹45");
+			"Price: 45";
 		
         else if (item=="Cheese") 
-			System.out.println("Price: ₹80");
+			"Price: 80";
 		
         else if (item=="Rice") 
-			System.out.println("Price: ₹70");
+			"Price: 70";
 		
         else if (item=="Wheat")
-			System.out.println("Price: ₹60");
+			"Price: 60";
 		
         else if (item=="Oil")
-			System.out.println("Price: ₹130");
+			"Price: 130";
 		
         else if (item=="Sugar") 
-			System.out.println("Price: ₹40");
+			"Price: 40";
 		
         else if (item=="Salt") 
-			System.out.println("Price: ₹20");
+			"Price: 20";
 		
         else if (item=="Toor Dal") 
-			System.out.println("Price: ₹90");
+			"Price: 90";
 		
         else if (item=="Moong Dal") 
-			System.out.println("Price: ₹95");
+			"Price: 95";
 		
         else if (item=="Tomato") 
-			System.out.println("Price: ₹25/kg");
+			"Price: 25/kg";
 		
         else if (item=="Onion") 
-			System.out.println("Price: ₹30/kg");
+			"Price: 30/kg";
 		
         else if (item=="Potato") 
-			System.out.println("Price: ₹22/kg");
+			"Price: 22/kg";
 		
         else if (item=="Garlic") 
-			System.out.println("Price: ₹80/kg");
+			"Price: 80/kg";
 		
         else if (item=="Ginger") 
-			System.out.println("Price: ₹60/kg");
+			"Price: 60/kg";
 		
         else if (item=="Green Chilli") 
-			System.out.println("Price: ₹40");
+			"Price: 40";
 		
         else if (item=="Apple") 
-			System.out.println("Price: ₹100/kg");
+			"Price: 100/kg";
 		
         else if (item=="Banana") 
-			System.out.println("Price: ₹40/dozen");
+			"Price: 40/dozen";
 		
         else if (item=="Mango") 
-			System.out.println("Price: ₹120/kg");
+			"Price: 120/kg";
 		
         else if (item=="Orange") 
-			System.out.println("Price: ₹60/kg");
+			"Price: 60/kg";
 		
         else if (item=="Grapes") 
-			System.out.println("Price: ₹70/kg");
+			"Price: 70/kg";
 		
         else if (item=="Lemon") 
-			System.out.println("Price: ₹10/4pcs");
+			"Price: 10/4pcs";
 		
         else if (item=="Coriander") 
-			System.out.println("Price: ₹10/bunch");
+			"Price: 10/bunch";
 		
         else if (item=="Mint") 
-			System.out.println("Price: ₹10/bunch");
+			"Price: 10/bunch";
 		
         else if (item=="Curd") 
-			System.out.println("Price: ₹40");
+			"Price: 40";
 		
         else if (item=="Paneer") 
-			System.out.println("Price: ₹90");
+			"Price: 90";
 		
         else if (item=="Ice Cream") 
-			System.out.println("Price: ₹100");
-		
+			"Price: 100;		
         else System.out.println("Item not available");
     
 	System.out.println("Item :"+item);
