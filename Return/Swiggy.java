0a4849d560252f033b10cfde853a582cd03ff8a0
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:47:12 +0530

    Update Swiggy.java

--- Return/Swiggy.java
+++ Return/Swiggy.java
@@ -3,90 +3,118 @@ class Swiggy {
 		int price = 0;
         if (dish=="Masala Dosa")
 			price=80;
+		return price;
 		
         else if (dish=="Paneer Tikka")
 			price=80;
+		return price;
 		
         else if (dish=="Butter Chicken")
 			price=200;
+		return price;
 		
         else if (dish="Chicken Biryani")
 			price=100;
+		return price;
 		
         else if (dish=="Veg Biryani")
 			price=200;
+		return price;
 		
         else if (dish=="Palak Paneer")
 			price=200;
+		return price;
 		
         else if (dish=="Dal Makhani")
 			price=150;
+		return price;
 		
         else if (dish=="Aloo Paratha")
 			price=80;
+		return price;
 		
         else if (dish=="Chole Bhature")
 			price=80;
-		
+		return price;
         else if (dish=="Rajma Chawal")
 			price=100;
+		return price;
 		
         else if (dish=="Naan")
 			price=70;
+		return price;
 		
         else if (dish=="Butter Naan")
 			price=90;
+		return price;
 		
         else if (dish=="Roti")
 			price=80;
+		return price;
 		
         else if (dish=="Fried Rice")
 			price=60;
+		return price;
 		
         else if (dish=="Gobi Manchurian")
 			price=80;
+		return price;
 		
         else if (dish=="Spring Roll")
 			price=80;
+		return price;
 		
         else if (dish=="Samosa")
 			price=50;
+		return price;
 		
         else if (dish=="Pakoda")
 			price=40;
+		return price;
 		
         else if (dish=="Pani Puri")
 			price=70;
+		return price;
 		
         else if (dish=="Pav Bhaji")
 			price=100;
+		return price;
 	
         else if (dish=="Pizza")
 			price=200;
+		return price;
 		
         else if (dish=="Burger")
 			price=200;
+		return price;
 		
         else if (dish=="French Fries")
 			price=300;
+		return price;
 		
         else if (dish=="Wrap")
 			price=80;
+		return price;
 		
         else if (dish=="Momos")
 			price=80;
+		return price;
 		
         else if (dish=="Ice Cream")
 			price=90;
+		return price;
 		
         else if (dish=="Cake")
 			price=70;
+		return price;
 		
         else if (dish=="Brownie")
 			price=80;
+		return price;
 		
         else if (dish=="Cold Coffee")
 			price=100;
+		return price;
 		
         else System.out.println("Dish not available");
 		
