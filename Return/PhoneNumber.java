d3362839e99f530d6ed3caaadb61d2c0b59a41d4
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:40:25 +0530

    Update PhoneNumber.java

--- Return/PhoneNumber.java
+++ Return/PhoneNumber.java
@@ -1,148 +1,148 @@
 class PhoneNumber {
 		
     public static long Number(String phno) {
-		
+		long num=0;
 		
         if (phno=="MyNumber")
-			System.out.println(8073932799l);
+			num=8073932799l;
 		
         else if (phno=="Pappa") 
-			System.out.println(9741994759l);
+			num=9741994759l;
 		
         else if (phno=="Amma")
-			System.out.println(8073932799l);
+			num=8073932799l;
 		
         else if (phno=="Dad") 
-			System.out.println(6364132177l);
+			num=6364132177l;
 		
         else if (phno=="amma") 
-			System.out.println(8073932799l);
+			num=8073932799l;
 		
         else if (phno=="Advi") 
-			System.out.println(9741565619l);
+			num=9741565619l;
 		
         else if (phno=="Linu")
-			System.out.println(8073139138l);
+			num=8073139138l;
 		
         else if (phno=="Shubha")
-			System.out.println(8073838139l);
+			num=8073838139l;
 		
         else if (phno=="Rakshu") 
-			System.out.println(8050525870l);
+			num=8050525870l;
 		
         else if (phno=="Ananya") 
-			System.out.println(9108488928l);
+			num=9108488928l;
 		
         else if (phno=="Anagha") 
-			System.out.println(8296841258l);
+			num=8296841258l;
 		
         else if (phno=="Appi") 
-			System.out.println(8197313730l);
+			num=8197313730l;
 		
         else if (phno=="Chikki") 
-			System.out.println(7026237589l);
+			num=7026237589l;
 		
         else if (phno=="Bhumika") 
-			System.out.println(9482269569l);
+			num=9482269569l;
 		
         else if (phno=="Bindu") 
-			System.out.println(9108427278l);
+			num=9108427278l;
 		
         else if (phno=="BindhuChikki") 
-			System.out.println(8147196459l);
+			num=8147196459l;
 		
         else if (phno=="Chandru Anna") 
-			System.out.println(9742914487l);
+			num=9742914487l;
 		
         else if (phno=="Dodamma") 
-			System.out.println(7795609546l);
+			num=7795609546l;
 		
         else if (phno=="Dodappa") 
-			System.out.println(9972484873l);
+			num=9972484873l;
 		
         else if (phno=="Putty") 
-			System.out.println(8090295853l);
+			num=8090295853l;
 		
         else if (phno=="Priya") 
-			System.out.println(9148072033l);
+			num=9148072033l;
 		
         else if (phno=="Preethi") 
-			System.out.println(7795495473l);
+			num=7795495473l;
 		
         else if (phno=="Pinky") 
-			System.out.println(7899477634l);
+			num=7899477634l;
 		
         else if (phno=="Pallu") 
-			System.out.println(9972458828l);
+			num=9972458828l;
 		
         else if (phno=="Nisarga") 
-			System.out.println(8792762885l);
+			num=8792762885l;
 		
         else if (phno=="Harshu") 
-			System.out.println(9613235443l);
+			num=9613235443l;
 		
         else if (phno=="Neha") 
-			System.out.println(8792291495l);
+			num=8792291495l;
 		
         else if (phno=="Naveen") 
-			System.out.println(88611632025l);
+			num=88611632025l;
 		
         else if (phno=="Nakshtra") 
-			System.out.println(8618423438l);
+			num=8618423438l;
 			
 		else if (phno=="Suma") 
-			System.out.println(8904239853l);
+			num=8904239853l;
 		
         else if (phno=="Meghna") 
-			System.out.println(8431595806l);
+			num=8431595806l;
 		
         else if (phno=="Chikku") 
-			System.out.println(8722855633l);
+			num=8722855633l;
 		
         else if (phno=="Thata") 
-			System.out.println(7483247146l);
+			num=7483247146l;
 		
         else if (phno=="Sanika") 
-			System.out.println(8762585678l);
+			num=8762585678l;
 		
         else if (phno=="Savita") 
-			System.out.println(8310165852l);
+			num=8310165852l;
 		
         else if (phno=="Navya") 
-			System.out.println(8073796941l);
+			num=8073796941l;
 		
         else if (phno=="Lokku Mama") 
-			System.out.println(9538596192l);
+			num=9538596192l;
 		
         else if (phno=="Latha2") 
-			System.out.println(7349740735l);
+			num=7349740735l;
 		
         else if (phno=="Latha") 
-			System.out.println(9482912568l);
+			num=9482912568l;
 		
 		else if (phno=="Raksh") 
-			System.out.println(8867049832l);
+			num=8867049832l;
 		
         else if (phno=="Ramya") 
-			System.out.println(8431092174l);
+			num=8431092174l;
 		
         else if (phno=="Ranjitha") 
-			System.out.println(8618612345l);
+			num=8618612345l;
 		
         else if (phno=="Rashmi") 
-			System.out.println(9008487204l);
+			num=9008487204l;
 		
         else if (phno=="Rohini") 
-			System.out.println(7019161361l);
+			num=7019161361l;
 		
         else if (phno=="Sana") 
-			System.out.println(7204847958l);
+			num=7204847958l;
 		
         else if (phno=="Sandhya") 
-			System.out.println(7892939554l);
+			num=7892939554l;
 		
         else if (phno=="Sanju") 
-			System.out.println(8861795069l);
+			num=8861795069l;
 		
         else System.out.println("Phone Number not available");
 
