c32190a36d391b50ff0635c216e79a3278be7dfe
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 10:43:38 +0530

    Update BigBasket.java

--- Return/BigBasket.java
+++ Return/BigBasket.java
@@ -1,93 +1,93 @@
 class BigBasket {
     public static void Avail(String item) {
-		
+		String Price=0;
         if (item=="Tomato Ketchup")
-			System.out.println("Price: ₹70");
+			"Price: 70";
 		
         else if (item=="Mayonnaise")
-			System.out.println("Price: ₹90");
+			"Price: 90";
 		
         else if (item=="Green Peas")
-			System.out.println("Price: ₹45");
+			"Price:45";
 		
         else if (item=="Frozen Corn")
-			System.out.println("Price: ₹60");
+			"Price: 60";
 		
         else if (item=="Basmati Rice")
-			System.out.println("Price: ₹120");
+			"Price: 120";
 		
         else if (item=="Brown Bread")
-			System.out.println("Price: ₹35");
+			"Price: 35";
 		
         else if (item=="Oats")
-			System.out.println("Price: ₹55");
+			"Price: 55";
 		
         else if (item=="Cornflakes")
-			System.out.println("Price: ₹80");
+			"Price: 80";
 		
         else if (item=="Chips")
-			System.out.println("Price: ₹20");
+			"Price: 20";
 		
         else if (item=="Biscuits")
-			System.out.println("Price: ₹30");
+			"Price: 30";
 		
         else if (item=="Juice Pack")
-			System.out.println("Price: ₹35");
+			"Price: 35";
 		
         else if (item=="Soft Drink")
-			System.out.println("Price: ₹40");
+			"Price: 40";
 		
         else if (item=="Water Bottle")
-			System.out.println("Price: ₹20");
+			"Price: 20";
 		
         else if (item=="Green Tea")
-			System.out.println("Price: ₹120");
+			"Price: 120";
 		
         else if (item=="Coffee")
-			System.out.println("Price: ₹150");
+			"Price: 150";
 		
         else if (item=="Tea Powder")
-			System.out.println("Price: ₹110");
+			"Price: 110";
 		
         else if (item=="Toothpaste")
-			System.out.println("Price: ₹50");
+			"Price: 50";
 		
         else if (item=="Shampoo")
-			System.out.println("Price: ₹90");
+			"Price: 90";
 		
         else if (item=="Soap")
-			System.out.println("Price: ₹35");
+			"Price: 35";
 		
 		
         else if (item=="Detergent")
-			System.out.println("Price: ₹95");
+			"Price:95";
 		
         else if (item=="Dishwash")
-			System.out.println("Price: ₹60");
+			"Price: 60";
 		
         else if (item=="Floor Cleaner")
-			System.out.println("Price: ₹85");
+			"Price: 85";
 		
         else if (item=="Handwash")
-			System.out.println("Price: ₹45");
+			"Price: 45";
 		
         else if (item=="Toilet Cleaner")
-			System.out.println("Price: ₹75");
+			"Price: 75";
 		
         else if (item=="Hair Oil")
-			System.out.println("Price: ₹65");
+			"Price: 65";
 		
         else if (item=="Body Lotion")
-			System.out.println("Price: ₹120");
+			"Price: 120";
 		
         else if (item=="Face Wash")
-			System.out.println("Price: ₹100");
+			"Price:100";
 		
         else if (item=="Face Mask")
-			System.out.println("Price: ₹50");
+			"Price: 50";
 		
         else if (item=="Hand Sanitizer")
-			System.out.println("Price: ₹40");
+			"Price: 40";
 		
         else System.out.println("Item not available");
 	
