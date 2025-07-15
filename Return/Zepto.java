6b93397a6f99371e664cb7f7704d873de4670258
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:49:12 +0530

    Update Zepto.java

--- Return/Zepto.java
+++ Return/Zepto.java
@@ -5,90 +5,96 @@ class Zepto {
 	String Price=null;
         if (item=="Milk")
 			"Price: 50";
+		return Price;
 		
         else if (item=="Bread") 
 			"Price: 30";
+		return Price;
 		
         else if (item=="Eggs")
 			"Price: 60";
+		return Price;
 		
         else if (item=="Butter") 
 			"Price: 45";
+		return Price;
 		
         else if (item=="Cheese") 
 			"Price: 80";
-		
+		return Price;
         else if (item=="Rice") 
 			"Price: 70";
-		
+		return Price;
         else if (item=="Wheat")
 			"Price: 60";
-		
+		return Price;
         else if (item=="Oil")
 			"Price: 130";
-		
+		return Price;
         else if (item=="Sugar") 
 			"Price: 40";
-		
+		return Price;
         else if (item=="Salt") 
 			"Price: 20";
-		
+		return Price;
         else if (item=="Toor Dal") 
 			"Price: 90";
-		
+		return Price;
         else if (item=="Moong Dal") 
 			"Price: 95";
-		
+		return Price;
         else if (item=="Tomato") 
 			"Price: 25/kg";
-		
+		return Price;
         else if (item=="Onion") 
 			"Price: 30/kg";
-		
+		return Price;
         else if (item=="Potato") 
 			"Price: 22/kg";
-		
+		return Price;
         else if (item=="Garlic") 
 			"Price: 80/kg";
-		
+		return Price;
         else if (item=="Ginger") 
 			"Price: 60/kg";
-		
+		return Price;
         else if (item=="Green Chilli") 
 			"Price: 40";
-		
+		return Price;
         else if (item=="Apple") 
 			"Price: 100/kg";
-		
+		return Price;
         else if (item=="Banana") 
 			"Price: 40/dozen";
-		
+		return Price;
         else if (item=="Mango") 
 			"Price: 120/kg";
-		
+		return Price;
         else if (item=="Orange") 
 			"Price: 60/kg";
-		
+		return Price;
         else if (item=="Grapes") 
 			"Price: 70/kg";
-		
+		return Price;
         else if (item=="Lemon") 
 			"Price: 10/4pcs";
-		
+		return Price;
         else if (item=="Coriander") 
 			"Price: 10/bunch";
-		
+		return Price;
         else if (item=="Mint") 
 			"Price: 10/bunch";
-		
+		return Price;
         else if (item=="Curd") 
 			"Price: 40";
-		
+		return Price;
         else if (item=="Paneer") 
 			"Price: 90";
 		
+		return Price;
         else if (item=="Ice Cream") 
-			"Price: 100;		
+			"Price: 100;
+		return Price;
         else System.out.println("Item not available");
     
 	System.out.println("Item :"+item);
