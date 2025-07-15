ec6758cb94d08f105f15af99dbe3ed02148666ce
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:31:45 +0530

    Update AmazonFresh.java

--- Return/AmazonFresh.java
+++ Return/AmazonFresh.java
@@ -3,91 +3,120 @@ class AmazonFresh {
     public static void Grocery(String grocery) {
 		String price=null;
         if (grocery=="Tomato")
-			price="₹30/kg";
+			price="30/kg";
+		return price;
 		
         else if (grocery=="Onion")
-			price="₹70/kg";
+			price="70/kg";
+		return price;
 		
         else if (grocery=="Potato")
-			price="₹37/kg";
+			price="37/kg";
+		return price;
 		
         else if (grocery=="Cucumber")
-			price="₹51/kg";
+			price="51/kg";
+		return price;
 		
         else if (grocery=="Carrot")
-			price="₹63/kg";
+			price="63/kg";
+		return price;
 		
         else if (grocery=="Bottle Gourd")
-			price="₹30/pc";
+			price="30/pc";
+		return price;
 		
         else if (grocery=="Brinjal")
-			price="₹40/kg";
+			price="40/kg";
+		return price;
 		
         else if (grocery=="Capsicum")
-			price="₹45/kg";
+			price="45/kg";
+		return price;
 		
         else if (grocery=="Beetroot")
-			price="₹40/kg";
+			price="40/kg";
+		return price;
 		
         else if (grocery=="Cauliflower")
-			price="₹35/pc";
+			price="35/pc";
+		return price;
 		
         else if (grocery=="Cabbage")
-			price="₹30/pc";
+			price="30/pc";
+		return price;
 		
         else if (grocery=="Chilli")
-			price="₹25/100g";
+			price="25/100g";
+		return price;
 		
         else if (grocery=="Beans")
-			price=₹58/kg";
+			price=58/kg";
+		return price;
 		
         else if (grocery=="Bitter Guard")
-			price="₹68/kg";
+			price="68/kg";
+		return price;
 		
         else if (grocery=="Ginger")
-			price=₹80/kg";
+			price=80/kg";
+		return price;
 		
         else if (grocery=="Pumpkin")
-			price="₹120/kg";
+			price="120/kg";
+		return price;
 		
         else if (grocery=="Broccoli")
-			price="₹60/kg";
+			price="60/kg";
+		return price;
 		
         else if (grocery=="Lettuce")
-			price="₹70/pc";
+			price="70/pc";
+		return price;
 		
         else if (grocery=="Orange")
-			price="₹60/kg";
+			price="60/kg";
+		return price;
 		
         else if (grocery=="Drumstick")
-			price="₹40/kg";
+			price="40/kg";
+		return price;
 		
         else if (grocery=="Parwal")
-			price="₹50/kg";
+			price="50/kg";
+		return price;
 		
         else if (grocery=="Avarakkai")
-			price="₹60/kg";
+			price="60/kg";
+		return price;
 		
         else if (grocery=="Ladies Finger")
-			price="₹80/kg";
+			price="80/kg";
+		return price;
 		
         else if (grocery=="Ghee")
-			price="₹120";
+			price="120";
+		return price;
 		
         else if (grocery=="Sugar")
-			price="₹40/kg";
+			price="40/kg";
+	    return price;
 		
         else if (grocery=="Coriendar Leaves")
-			price="₹35";
+			price="35";
+		return price;
 		
         else if (grocery=="Rice")
-			price="₹70/kg";
+			price="70/kg";
+		return price;
 		
         else if (grocery=="Atta")
-			price="₹60/kg";
+			price="60/kg";
+		return price;
 		
         else if (grocery=="Toor Dal")
-			price="₹100/kg";
+			price="100/kg";
+		return price;
 		
         else System.out.println("Item not available");
 		
