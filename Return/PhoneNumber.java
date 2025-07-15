a8364029467bdf2c68432adf9b68ec32eb9c77ea
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:43:18 +0530

    Update PhoneNumber.java

--- Return/PhoneNumber.java
+++ Return/PhoneNumber.java
@@ -5,144 +5,190 @@ class PhoneNumber {
 		
         if (phno=="MyNumber")
 			num=8073932799l;
+		return num;
 		
         else if (phno=="Pappa") 
 			num=9741994759l;
+		return num;
 		
         else if (phno=="Amma")
 			num=8073932799l;
+		return num;
 		
         else if (phno=="Dad") 
 			num=6364132177l;
+		return num;
 		
         else if (phno=="amma") 
 			num=8073932799l;
+		return num;
 		
         else if (phno=="Advi") 
 			num=9741565619l;
+		return num;
 		
         else if (phno=="Linu")
 			num=8073139138l;
+		return num;
 		
         else if (phno=="Shubha")
 			num=8073838139l;
+		return num;
 		
         else if (phno=="Rakshu") 
 			num=8050525870l;
+		return num;
 		
         else if (phno=="Ananya") 
 			num=9108488928l;
+		return num;
 		
         else if (phno=="Anagha") 
 			num=8296841258l;
+		return num;
 		
         else if (phno=="Appi") 
 			num=8197313730l;
+		return num;
 		
         else if (phno=="Chikki") 
 			num=7026237589l;
+		return num;
 		
         else if (phno=="Bhumika") 
 			num=9482269569l;
+		return num;
 		
         else if (phno=="Bindu") 
 			num=9108427278l;
+		return num;
 		
         else if (phno=="BindhuChikki") 
 			num=8147196459l;
+		return num;
 		
         else if (phno=="Chandru Anna") 
 			num=9742914487l;
+		return num;
 		
         else if (phno=="Dodamma") 
 			num=7795609546l;
+		return num;
 		
         else if (phno=="Dodappa") 
 			num=9972484873l;
+		return num;
 		
         else if (phno=="Putty") 
 			num=8090295853l;
+		return num;
 		
         else if (phno=="Priya") 
 			num=9148072033l;
+		return num;
 		
         else if (phno=="Preethi") 
 			num=7795495473l;
+		return num;
 		
         else if (phno=="Pinky") 
 			num=7899477634l;
+		return num;
 		
         else if (phno=="Pallu") 
 			num=9972458828l;
+		return num;
 		
         else if (phno=="Nisarga") 
 			num=8792762885l;
+		return num;
 		
         else if (phno=="Harshu") 
 			num=9613235443l;
+		return num;
 		
         else if (phno=="Neha") 
 			num=8792291495l;
+		return num;
 		
         else if (phno=="Naveen") 
 			num=88611632025l;
+		return num;
 		
         else if (phno=="Nakshtra") 
 			num=8618423438l;
+		return num;
 			
 		else if (phno=="Suma") 
 			num=8904239853l;
+			return num;
 		
         else if (phno=="Meghna") 
 			num=8431595806l;
+		return num;
 		
         else if (phno=="Chikku") 
 			num=8722855633l;
+		return num;
 		
         else if (phno=="Thata") 
 			num=7483247146l;
+		return num;
 		
         else if (phno=="Sanika") 
 			num=8762585678l;
+		return num;
 		
         else if (phno=="Savita") 
 			num=8310165852l;
+		return num;
 		
         else if (phno=="Navya") 
 			num=8073796941l;
-		
+		return num;
         else if (phno=="Lokku Mama") 
 			num=9538596192l;
+		return num;
 		
         else if (phno=="Latha2") 
 			num=7349740735l;
+		return num;
 		
         else if (phno=="Latha") 
 			num=9482912568l;
+		return num;
 		
 		else if (phno=="Raksh") 
 			num=8867049832l;
+			return num;
 		
         else if (phno=="Ramya") 
 			num=8431092174l;
+		return num;
 		
         else if (phno=="Ranjitha") 
 			num=8618612345l;
+		return num;
 		
         else if (phno=="Rashmi") 
 			num=9008487204l;
+		return num;
 		
         else if (phno=="Rohini") 
 			num=7019161361l;
+		return num;
 		
         else if (phno=="Sana") 
 			num=7204847958l;
+		return num;
 		
         else if (phno=="Sandhya") 
 			num=7892939554l;
+		return num;
 		
         else if (phno=="Sanju") 
 			num=8861795069l;
+		return num;
 		
         else System.out.println("Phone Number not available");
 
