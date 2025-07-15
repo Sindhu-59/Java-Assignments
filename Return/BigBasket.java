bb8175913f5fb5f4703ef15c0ef1ff02286014c6
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:33:43 +0530

    Update BigBasket.java

--- Return/BigBasket.java
+++ Return/BigBasket.java
@@ -3,91 +3,120 @@ class BigBasket {
 		String Price=0;
         if (item=="Tomato Ketchup")
 			"Price: 70";
+		return Price;
 		
         else if (item=="Mayonnaise")
 			"Price: 90";
+		return Price;
 		
         else if (item=="Green Peas")
 			"Price:45";
+		return Price;
 		
         else if (item=="Frozen Corn")
 			"Price: 60";
+		return Price;
 		
         else if (item=="Basmati Rice")
 			"Price: 120";
+		return Price;
 		
         else if (item=="Brown Bread")
 			"Price: 35";
+		return Price;
 		
         else if (item=="Oats")
 			"Price: 55";
+		return Price;
 		
         else if (item=="Cornflakes")
 			"Price: 80";
+		return Price;
 		
         else if (item=="Chips")
 			"Price: 20";
+		return Price;
 		
         else if (item=="Biscuits")
 			"Price: 30";
+		return Price;
 		
         else if (item=="Juice Pack")
 			"Price: 35";
+		return Price;
 		
         else if (item=="Soft Drink")
 			"Price: 40";
+		return Price;
 		
         else if (item=="Water Bottle")
 			"Price: 20";
+		return Price;
 		
         else if (item=="Green Tea")
 			"Price: 120";
+		return Price;
 		
         else if (item=="Coffee")
 			"Price: 150";
+		return Price;
 		
         else if (item=="Tea Powder")
 			"Price: 110";
+		return Price;
 		
         else if (item=="Toothpaste")
 			"Price: 50";
+		return Price;
 		
         else if (item=="Shampoo")
 			"Price: 90";
+		return Price;
 		
         else if (item=="Soap")
 			"Price: 35";
+		return Price;
 		
 		
         else if (item=="Detergent")
 			"Price:95";
+		return Price;
 		
         else if (item=="Dishwash")
 			"Price: 60";
+		return Price;
 		
         else if (item=="Floor Cleaner")
 			"Price: 85";
+		return Price;
 		
         else if (item=="Handwash")
 			"Price: 45";
+		return Price;
 		
         else if (item=="Toilet Cleaner")
 			"Price: 75";
+		return Price;
 		
         else if (item=="Hair Oil")
 			"Price: 65";
+		return Price;
 		
         else if (item=="Body Lotion")
 			"Price: 120";
+		return Price;
 		
         else if (item=="Face Wash")
 			"Price:100";
+		return Price;
 		
         else if (item=="Face Mask")
 			"Price: 50";
+		return Price;
 		
         else if (item=="Hand Sanitizer")
 			"Price: 40";
+		return Price;
 		
         else System.out.println("Item not available");
 	
