66916e800bf52588fe52065215ea19c903c5b86d
Sindhu-59 <sindhumanjunath2003@gmail.com>
2025-07-15 11:22:16 +0530

    Update Country.java

--- Return/Country.java
+++ Return/Country.java
@@ -10,33 +10,43 @@ class Country{
 		}
 		else if(CountryName=="Australia"){
 			CountryCode=61;
+			return CountryCode;
 			}
 			else if(CountryName=="Sri Lanka"){
 				CountryCode=94;
+				return CountryCode;
 				}
 				else if(CountryName=="Singapore"){
 				CountryCode=65;
+					return CountryCode;
 				}
 				else if(CountryName=="Spain"){
 				CountryCode=34;
+					return CountryCode;
 				}
 				else if(CountryName=="Sweden"){
 				CountryCode=46;
+					return CountryCode;
 				}
 				else if(CountryName=="Bangladesh"){
 				CountryCode=880;
+					return CountryCode;
 				}
 				else if(CountryName=="Bhuthan"){
 				CountryCode=975;
+					return CountryCode;
 				}
 				else if(CountryName=="Cuba"){
 				CountryCode=53;
+					return CountryCode;
 				}
 				else if(CountryName=="South Korea"){
 				CountryCode=82;
+					return CountryCode;
 				}
 				else if(CountryName=="Somoa"){
 				CountryCode=685;
+					return CountryCode;
 				}
 				
 				else{
