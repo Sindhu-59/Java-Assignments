8f7c2d21a63ae25a5f8400eacc044382341e93c2
Sindhu <sindhumanjunath2003@gmail.com>
2025-07-17 11:34:55 +0530

    sop

--- Return/Country.java
+++ Return/Country.java
@@ -1,13 +1,11 @@
-class Country{
+class Country {
+    public static int getCountryCode(String countryName){
+        int countryCode=0;
 
-	public static int getCountryCode(String CountryName){
-	
-	int CountryCode = 0;
-	
-		
-	if(CountryName=="India"){
-		CountryCode=91;
+        if(countryName=="Afghanistan"){
+			countryCode=93; 
 		}
+<<<<<<< HEAD
 		else if(CountryName=="Australia"){
 			CountryCode=61;
 			return CountryCode;
@@ -53,6 +51,755 @@ class Country{
 					System.out.println("The country code is invalid "+CountryCode);
 					}
 			return CountryCode;
+=======
+        else if(countryName=="Aland"){ 
+		countryCode=358; 
 		}
+        else if(countryName=="Albania"){
+			countryCode=355;
 		}
+        else if(countryName=="Algeria"){ 
+		countryCode=213; 
+		}
+        else if(countryName=="Angola"){
+			countryCode=244;
+			}
+        else if(countryName=="Anguilla"){ 
+		countryCode=1264;
+>>>>>>> 2e54b40 (sop)
+		}
+        else if(countryName=="Antigua and Barbuda"){ 
+		countryCode=1268; 
+		}
+<<<<<<< HEAD
 		
+=======
+        else if(countryName=="Argentina"){ 
+		countryCode=54;
+		}
+        else if(countryName=="Armenia"){ 
+		countryCode=374;
+		}
+        else if(countryName=="Aruba"){ 
+		countryCode=297; 
+		}
+        else if(countryName=="Ascension"){
+			countryCode=247;
+			}
+        else if(countryName=="Australia"){
+			countryCode=61; 
+			}
+        else if(countryName=="Austria"){ 
+		countryCode=43; 
+		}
+        else if(countryName=="Azerbaijan"){ 
+		countryCode=994; 
+		}
+        else if(countryName=="Bahamas"){ 
+		countryCode=1242; 
+		}
+        else if(countryName=="Bahrain"){
+			countryCode=973;
+			}
+        else if(countryName=="Bangladesh"){
+			countryCode=880; 
+			}
+        else if(countryName=="Barbados"){ 
+		countryCode=1246; 
+		}
+        else if(countryName=="Belarus"){ 
+		countryCode=375; 
+		}
+        else if(countryName=="Belgium"){ 
+		countryCode=32;
+		}
+        else if(countryName=="Belize"){ 
+		countryCode=501; 
+		}
+        else if(countryName=="Benin"){ 
+		countryCode=229; 
+		}
+        else if(countryName=="Bermuda"){ 
+		countryCode=1441; 
+		}
+        else if(countryName=="Bhutan"){ 
+		countryCode=975; 
+		}
+        else if(countryName=="Bolivia"){
+			countryCode=591; 
+			}
+        else if(countryName=="Bonaire"){ 
+		countryCode=599; 
+		}
+        else if(countryName=="Bosnia and Herzegovina"){
+			countryCode=387; 
+			}
+        else if(countryName=="Botswana"){ 
+		countryCode=267; 
+		}
+        else if(countryName=="Brazil"){ 
+		countryCode=55; 
+		}
+        else if(countryName=="British Indian Ocean Territory"){ 
+		countryCode=246; 
+		}
+        else if(countryName=="British Virgin Islands"){ 
+		countryCode=1284; 
+		}
+        else if(countryName=="Brunei Darussalam"){ 
+		countryCode=673;
+		}
+        else if(countryName=="Bulgaria"){ 
+		countryCode=359;
+		}
+        else if(countryName=="Burkina Faso"){ 
+		countryCode=226;
+		}
+        else if(countryName=="Burundi"){ 
+		countryCode=257;
+		}
+        else if(countryName=="Cambodia"){ 
+		countryCode=855; 
+		}
+        else if(countryName=="Cameroon"){ 
+		countryCode=237; 
+		}
+        else if(countryName=="Canada"){ 
+		countryCode=1; 
+		}
+        else if(countryName=="Cape Verde"){ 
+		countryCode=238; 
+		}
+        else if(countryName=="Caribbean Netherlands"){ 
+		countryCode=599; 
+		}
+        else if(countryName=="Cayman Islands"){ 
+		countryCode=1345;
+		}
+        else if(countryName=="Central African Republic"){ 
+		countryCode=236; 
+		}
+        else if(countryName=="Chad"){ 
+		countryCode=235; 
+		}
+        else if(countryName=="Chatham Island, New Zealand"){ 
+		countryCode=64; 
+		}
+        else if(countryName=="Chile"){
+			countryCode=56; 
+			}
+        else if(countryName=="China"){ 
+		countryCode=86; 
+		}
+        else if(countryName=="Christmas Island"){
+			countryCode=61; 
+			}
+        else if(countryName=="Cocos (Keeling) Islands"){ 
+		countryCode=61; 
+		}
+        else if(countryName=="Colombia"){ 
+		countryCode=57;
+		}
+        else if(countryName=="Comoros"){ 
+		countryCode=269; 
+		}
+        else if(countryName=="Congo"){ 
+		countryCode=242;
+		}
+        else if(countryName=="Congo, Democratic Republic of the"){ 
+		countryCode=243; 
+		}
+        else if(countryName=="Cook Islands"){ 
+		countryCode=682; 
+		}
+        else if(countryName=="Costa Rica"){ 
+		countryCode=506; 
+		}
+        else if(countryName=="Croatia"){ 
+		countryCode=385; 
+		}
+        else if(countryName=="Cuba"){ 
+		countryCode=53;
+		}
+        else if(countryName=="Curaçao"){ 
+		countryCode=599; 
+		}
+        else if(countryName=="Cyprus"){ 
+		countryCode=357; 
+		}
+        else if(countryName=="Czech Republic"){ 
+		countryCode=420; 
+		}
+        else if(countryName=="Denmark"){ 
+		countryCode=45; 
+		}
+        else if(countryName=="Djibouti"){ 
+		countryCode=253; 
+		}
+        else if(countryName=="Dominica"){ 
+		countryCode=1767; 
+		}
+        else if(countryName=="Dominican Republic"){ 
+		countryCode=1809; 
+		}
+        else if(countryName=="Ecuador"){ 
+		countryCode=593;
+		}
+        else if(countryName=="Egypt"){
+			countryCode=20;
+			}
+        else if(countryName=="El Salvador"){ 
+		countryCode=503;
+		}
+        else if(countryName=="Equatorial Guinea"){ 
+		countryCode=240;
+		}
+        else if(countryName=="Eritrea"){ 
+		countryCode=291; 
+		}
+        else if(countryName=="Estonia"){ 
+		countryCode=372; 
+		}
+        else if(countryName=="Eswatini"){ 
+		countryCode=268;
+		}
+        else if(countryName=="Ethiopia"){
+			countryCode=251;
+			}
+        else if(countryName=="Fiji"){
+			countryCode=679;
+			}
+        else if(countryName=="Finland"){ 
+		countryCode=358; 
+		}
+        else if(countryName=="France"){ 
+		countryCode=33;
+		}
+        else if(countryName=="French Guiana"){
+			countryCode=594; 
+			}
+        else if(countryName=="French Polynesia"){
+			countryCode=689;
+			}
+        else if(countryName=="Gabon"){ 
+		countryCode=241; 
+		}
+        else if(countryName=="Gambia"){ 
+		countryCode=220; 
+		}
+        else if(countryName=="Georgia"){ 
+		countryCode=995;
+		}
+        else if(countryName=="Germany"){ 
+		countryCode=49;
+		}
+        else if(countryName=="Ghana"){ 
+		countryCode=233;
+		}
+        else if(countryName=="Gibraltar"){ 
+		countryCode=350;
+		}
+        else if(countryName=="Greece"){ 
+		countryCode=30; 
+		}
+        else if(countryName=="Greenland"){ 
+		countryCode=299; 
+		}
+        else if(countryName=="Grenada"){ 
+		countryCode=1473;
+		}
+        else if(countryName=="Guadeloupe"){ 
+		countryCode=590;
+		}
+        else if(countryName=="Guam"){ 
+		countryCode=1671;
+		}
+        else if(countryName=="Guatemala"){ 
+		countryCode=502; 
+		}
+        else if(countryName=="Guernsey"){ 
+		countryCode=44;
+		}
+        else if(countryName=="Guinea"){ 
+		countryCode=224; 
+		}
+        else if(countryName=="Guinea-Bissau"){ 
+		countryCode=245; 
+		}
+        else if(countryName=="Guyana"){ 
+		countryCode=592;
+		}
+        else if(countryName=="Haiti"){ 
+		countryCode=509; 
+		}
+        else if(countryName=="Honduras"){ 
+		countryCode=504; 
+		}
+        else if(countryName=="Hong Kong"){ 
+		countryCode=852; 
+		}
+        else if(countryName=="Hungary"){ 
+		countryCode=36; 
+		}
+        else if(countryName=="Iceland"){ 
+		countryCode=354; 
+		}
+        else if(countryName=="India"){ 
+		countryCode=91; 
+		}
+        else if(countryName=="Indonesia"){ 
+		countryCode=62;
+		}
+        else if(countryName=="Iran"){ 
+		countryCode=98; 
+		}
+        else if(countryName=="Iraq"){ 
+		countryCode=964;
+		}
+        else if(countryName=="Ireland"){ 
+		countryCode=353; 
+		}
+        else if(countryName=="Isle of Man"){ 
+		countryCode=44; 
+		}
+        else if(countryName=="Israel"){ 
+		countryCode=972; 
+		}
+        else if(countryName=="Italy"){ 
+		countryCode=39;
+		}
+        else if(countryName=="Ivory Coast (Côte d'Ivoire)"){
+			countryCode=225;
+			}
+        else if(countryName=="Jamaica"){
+			countryCode=1876; 
+			}
+        else if(countryName=="Japan"){ 
+		countryCode=81;
+		}
+        else if(countryName=="Jersey"){
+			countryCode=44;
+			}
+        else if(countryName=="Jordan"){ 
+		countryCode=962; 
+		}
+        else if(countryName=="Kazakhstan"){
+			countryCode=7;
+			}
+        else if(countryName=="Kenya"){ 
+		countryCode=254; 
+		}
+        else if(countryName=="Kiribati"){ 
+		countryCode=686;
+		}
+        else if(countryName=="Korea, North"){
+			countryCode=850; 
+			}
+        else if(countryName=="Korea, South"){ 
+		countryCode=82; 
+		}
+        else if(countryName=="Kosovo"){ 
+		countryCode=383;
+		}
+        else if(countryName=="Kuwait"){
+			countryCode=965;
+			}
+        else if(countryName=="Kyrgyzstan"){ 
+		countryCode=996; 
+		}
+        else if(countryName=="Laos"){ 
+		countryCode=856; 
+		}
+        else if(countryName=="Latvia"){ 
+		countryCode=371; 
+		}
+        else if(countryName=="Lebanon"){
+			countryCode=961;
+			}
+        else if(countryName=="Lesotho"){ 
+		countryCode=266; 
+		}
+        else if(countryName=="Liberia"){ 
+		countryCode=231; 
+		}
+        else if(countryName=="Libya"){ 
+		countryCode=218; 
+		}
+        else if(countryName=="Liechtenstein"){ 
+		countryCode=423; 
+		}
+        else if(countryName=="Lithuania"){ 
+		countryCode=370; 
+		}
+        else if(countryName=="Luxembourg"){ 
+		countryCode=352; 
+		}
+        else if(countryName=="Macau"){
+			countryCode=853;
+			}
+        else if(countryName=="Madagascar"){ 
+		countryCode=261;
+		}
+        else if(countryName=="Malawi"){ 
+		countryCode=265; 
+		}
+        else if(countryName=="Malaysia"){ 
+		countryCode=60; 
+		}
+        else if(countryName=="Maldives"){ 
+		countryCode=960;
+		}
+        else if(countryName=="Mali"){ 
+		countryCode=223; 
+		}
+        else if(countryName=="Malta"){ 
+		countryCode=356;
+		}
+        else if(countryName=="Marshall Islands"){ 
+		countryCode=692; 
+		}
+        else if(countryName=="Martinique"){ 
+		countryCode=596; 
+		}
+        else if(countryName=="Mauritania"){ 
+		countryCode=222; 
+		}
+        else if(countryName=="Mauritius"){ 
+		countryCode=230; 
+		}
+        else if(countryName=="Mayotte"){
+			countryCode=262;
+			}
+        else if(countryName=="Mexico"){
+			countryCode=52;
+			}
+        else if(countryName=="Micronesia, Federated States of"){ 
+		countryCode=691;
+		}
+        else if(countryName=="Moldova"){ 
+		countryCode=373; 
+		}
+        else if(countryName=="Monaco"){ 
+		countryCode=377; 
+		}
+        else if(countryName=="Mongolia"){ 
+		countryCode=976; 
+		}
+        else if(countryName=="Montenegro"){ 
+		countryCode=382; 
+		}
+        else if(countryName=="Montserrat"){ 
+		countryCode=1664; 
+		}
+        else if(countryName=="Morocco"){ 
+		countryCode=212; 
+		}
+        else if(countryName=="Mozambique"){ 
+		countryCode=258; 
+		}
+        else if(countryName=="Myanmar"){
+			countryCode=95;
+			}
+        else if(countryName=="Namibia"){
+			countryCode=264; 
+			}
+        else if(countryName=="Nauru"){ 
+		countryCode=674; 
+		}
+        else if(countryName=="Nepal"){ 
+		countryCode=977; 
+		}
+        else if(countryName=="Netherlands"){ 
+		countryCode=31; 
+		}
+        else if(countryName=="New Caledonia"){
+			countryCode=687; 
+			}
+        else if(countryName=="New Zealand"){ 
+		countryCode=64; 
+		}
+        else if(countryName=="Nicaragua"){
+			countryCode=505; 
+			}
+        else if(countryName=="Niger"){
+			countryCode=227;
+			}
+        else if(countryName=="Nigeria"){ 
+		countryCode=234; 
+		}
+        else if(countryName=="Niue"){ 
+		countryCode=683;
+		}
+        else if(countryName=="Norfolk Island"){ 
+		countryCode=672; 
+		}
+        else if(countryName=="North Macedonia"){ 
+		countryCode=389; 
+		}
+        else if(countryName=="Northern Cyprus"){ 
+		countryCode=90;
+		}
+        else if(countryName=="Northern Ireland"){ 
+		countryCode=44;
+		}
+        else if(countryName=="Northern Mariana Islands"){ 
+		countryCode=1670; 
+		}
+        else if(countryName=="Norway"){
+			countryCode=47; 
+			}
+		        else if(countryName=="Oman"){
+					countryCode=968; }
+        else if(countryName=="Pakistan"){
+			countryCode=92; 
+			}
+        else if(countryName=="Palau"){
+			countryCode=680; 
+			}
+        else if(countryName=="Palestine, State of"){ 
+		countryCode=970; 
+		}
+        else if(countryName=="Panama"){ 
+		countryCode=507;
+		}
+        else if(countryName=="Papua New Guinea"){
+			countryCode=675; 
+			}
+        else if(countryName=="Paraguay"){
+			countryCode=595; 
+			}
+        else if(countryName=="Peru"){
+			countryCode=51;
+			}
+        else if(countryName=="Philippines"){ 
+		countryCode=63; }
+        else if(countryName=="Pitcairn Islands"){
+			countryCode=64; 
+			}
+        else if(countryName=="Poland"){
+			countryCode=48;
+			}
+        else if(countryName=="Portugal"){ 
+		countryCode=351;
+		}
+        else if(countryName=="Puerto Rico"){ 
+		countryCode=1787;
+		}
+        else if(countryName=="Qatar"){ 
+		countryCode=974; 
+		}
+        else if(countryName=="Réunion"){ 
+		countryCode=262;
+		}
+        else if(countryName=="Romania"){ 
+		countryCode=40; 
+		}
+        else if(countryName=="Russia"){ 
+		countryCode=7; 
+		}
+        else if(countryName=="Rwanda"){ 
+		countryCode=250; 
+		}
+        else if(countryName=="Saba"){ 
+		countryCode=599; 
+		}
+        else if(countryName=="Saint Barthélemy"){ 
+		countryCode=590; 
+		}
+        else if(countryName=="Saint Helena"){
+			countryCode=290;
+			}
+        else if(countryName=="Saint Kitts and Nevis"){
+			countryCode=1869; }
+        else if(countryName=="Saint Lucia"){ 
+		countryCode=1758; 
+		}
+        else if(countryName=="Saint Martin (France)"){
+			countryCode=590; 
+			}
+        else if(countryName=="Saint Pierre and Miquelon"){ 
+		countryCode=508; 
+		}
+        else if(countryName=="Saint Vincent and the Grenadines"){
+			countryCode=1784; 
+			}
+        else if(countryName=="Samoa"){ 
+		countryCode=685; }
+        else if(countryName=="San Marino"){ 
+		countryCode=378;
+		}
+        else if(countryName=="São Tomé and Príncipe"){ 
+		countryCode=239; 
+		}
+        else if(countryName=="Saudi Arabia"){ 
+		countryCode=966; 
+		}
+        else if(countryName=="Senegal"){ 
+		countryCode=221;
+		}
+        else if(countryName=="Serbia"){ 
+		countryCode=381; 
+		}
+        else if(countryName=="Seychelles"){ 
+		countryCode=248; 
+		}
+        else if(countryName=="Sierra Leone"){ 
+		countryCode=232; 
+		}
+        else if(countryName=="Singapore"){ 
+		countryCode=65; 
+		}
+        else if(countryName=="Sint Eustatius"){ 
+		countryCode=599;
+		}
+        else if(countryName=="Sint Maarten (Netherlands)"){ 
+		countryCode=599; 
+		}
+        else if(countryName=="Slovakia"){
+			countryCode=421; 
+			}
+        else if(countryName=="Slovenia"){ 
+		countryCode=386; }
+        else if(countryName=="Solomon Islands"){
+			countryCode=677; 
+			}
+        else if(countryName=="Somalia"){ 
+		countryCode=252; 
+		}
+        else if(countryName=="South Africa"){ 
+		countryCode=27;
+		}
+        else if(countryName=="South Georgia and the South Sandwich Islands"){ 
+		countryCode=500; 
+		}
+        else if(countryName=="South Ossetia"){ 
+		countryCode=995; 
+		}
+        else if(countryName=="South Sudan"){
+			countryCode=211;
+			}
+        else if(countryName=="Spain"){ 
+		countryCode=34;
+		}
+        else if(countryName=="Sri Lanka"){ 
+		countryCode=94; 
+		}
+        else if(countryName=="Sudan"){ 
+		countryCode=249;
+		}
+        else if(countryName=="Suriname"){ 
+		countryCode=597;
+		}
+        else if(countryName=="Svalbard"){ 
+		countryCode=47; 
+		}
+        else if(countryName=="Sweden"){ 
+		countryCode=46;
+		}
+        else if(countryName=="Switzerland"){ 
+		countryCode=41; 
+		}
+        else if(countryName=="Syria"){
+			countryCode=963; 
+		}
+        else if(countryName=="Taiwan"){ 
+		countryCode=886;
+		}
+        else if(countryName=="Tajikistan"){ 
+		countryCode=992;
+		}
+        else if(countryName=="Tanzania"){ 
+		countryCode=255; 
+		}
+        else if(countryName=="Thailand"){ 
+		countryCode=66; }
+        else if(countryName=="Togo"){
+			countryCode=228;
+			}
+        else if(countryName=="Tokelau"){ 
+		countryCode=690;
+		}
+        else if(countryName=="Tonga"){ 
+		countryCode=676; 
+		}
+        else if(countryName=="Transnistria"){ 
+		countryCode=373; 
+		}
+        else if(countryName=="Trinidad and Tobago"){ 
+		countryCode=1868; 
+		}
+        else if(countryName=="Tristan da Cunha"){
+			countryCode=290; 
+			}
+        else if(countryName=="Tunisia"){ 
+		countryCode=216;
+		}
+        else if(countryName=="Turkey"){ 
+		countryCode=90; 
+		}
+        else if(countryName=="Turkmenistan"){ 
+		countryCode=993; 
+		}
+        else if(countryName=="Turks and Caicos Islands"){ 
+		countryCode=1649; 
+		}
+        else if(countryName=="Tuvalu"){ 
+		countryCode=688;
+		}
+        else if(countryName=="Uganda"){ 
+		countryCode=256; 
+		}
+        else if(countryName=="Ukraine"){ 
+		countryCode=380;
+		}
+        else if(countryName=="United Arab Emirates"){ 
+		countryCode=971;
+		}
+        else if(countryName=="United Kingdom"){ 
+		countryCode=44; 
+		}
+        else if(countryName=="United States"){ 
+		countryCode=1;
+		}
+        else if(countryName=="Uruguay"){
+			countryCode=598;
+			}
+        else if(countryName=="US Virgin Islands"){
+			countryCode=1340;
+			}
+        else if(countryName=="Uzbekistan"){
+			countryCode=998;
+			}
+        else if(countryName=="Vanuatu"){ 
+		countryCode=678; 
+		}
+        else if(countryName=="Vatican City State (Holy See)"){
+			countryCode=379; 
+			}
+        else if(countryName=="Venezuela"){ 
+		countryCode=58; 
+		}
+        else if(countryName=="Vietnam"){ 
+		countryCode=84;
+		}
+        else if(countryName=="Wallis and Futuna"){
+			countryCode=681; 
+			}
+        else if(countryName=="Yemen"){ 
+		countryCode=967;
+		}
+        else if(countryName=="Zambia"){ 
+		countryCode=260;
+		}
+        else if(countryName=="Zanzibar"){
+			countryCode=255;
+			}
+        else if(countryName=="Zimbabwe"){ 
+		countryCode=263;
+		}
+        System.out.println("Country: " + countryName);
+        System.out.println("Country Code: +" + countryCode);
+        return countryCode;
+    }
+}
+>>>>>>> 2e54b40 (sop)
--- Return/Zepto.java
+++ Return/Zepto.java
@@ -2,6 +2,7 @@ class Zepto {
 		
     public static void order(String item) {
 		
+<<<<<<< HEAD
 	String Price=null;
         if (item=="Milk")
 			"Price: 50";
@@ -90,13 +91,201 @@ class Zepto {
 		return Price;
         else if (item=="Paneer") 
 			"Price: 90";
+=======
+		int Price=0;
+        if (item=="Dairy Milk")
+			Price: ₹50;
+		
+        else if (item=="Bread") 
+			Price: ₹30;
+		
+        else if (item=="Eggs")
+			Price: ₹60;
+		
+        else if (item=="Butter") 
+			Price: ₹45;
+		
+        else if (item=="Cheese") 
+			Price: ₹80;
+		
+        else if (item=="Rice") 
+			Price: ₹70;
+		
+        else if (item=="Wheat")
+			Price: ₹60;
+		
+        else if (item=="Oil")
+			Price: ₹130;
+		
+        else if (item=="Sugar") 
+			Price: ₹40;
+		
+        else if (item=="Salt") 
+			Price: ₹20;
+		
+        else if (item=="Toor Dal") 
+			Price: ₹90;
+		
+        else if (item=="Moong Dal") 
+			Price: ₹95;
+		
+        else if (item=="Tomato") 
+			Price: ₹25;
+		
+        else if (item=="Onion") 
+			Price: ₹30;
+		
+        else if (item=="Potato") 
+			Price: ₹22;
+		
+        else if (item=="Garlic") 
+			Price: ₹80;
+		
+        else if (item=="Ginger") 
+			Price: ₹60;
+		
+        else if (item=="Green Chilli") 
+			Price: ₹40;
+		
+        else if (item=="Apple") 
+			Price: ₹100;
+		
+        else if (item=="Banana") 
+			Price: ₹40;
+		
+        else if (item=="Mango") 
+			Price: ₹120;
+		
+        else if (item=="Orange") 
+			Price: ₹60;
+		
+        else if (item=="Grapes") 
+			Price: ₹70;
+		
+        else if (item=="Lemon") 
+			Price: ₹10;
+		
+        else if (item=="Coriander") 
+			Price: ₹10;
+		
+        else if (item=="Mint") 
+			Price: ₹10;
+		
+        else if (item=="Curd") 
+			Price: ₹40;
+		
+        else if (item=="Paneer") 
+			Price: ₹90;
+>>>>>>> 2e54b40 (sop)
 		
 		return Price;
         else if (item=="Ice Cream") 
+<<<<<<< HEAD
 			"Price: 100;
 		return Price;
+=======
+			Price: 100;
+		
+>>>>>>> 2e54b40 (sop)
         else System.out.println("Item not available");
     
 	System.out.println("Item :"+item);
+	return Price;
 }
+	
+	public static void order(String item,int quantity){
+		
+	int Price=0;
+        if (item=="Dairy Milk")
+			Price: 50*quantity;
+		
+        else if (item=="Bread") 
+			Price: 30*quantity;
+		
+        else if (item=="Eggs")
+			Price: 60*quantity;
+		
+        else if (item=="Butter") 
+			Price: 45*quantity;
+		
+        else if (item=="Cheese") 
+			Price: 80*quantity;
+		
+        else if (item=="Rice") 
+			Price: 70*quantity;
+		
+        else if (item=="Wheat")
+			Price: 60*quantity;
+		
+        else if (item=="Oil")
+			Price: 130*quantity;
+		
+        else if (item=="Sugar") 
+			Price: 40*quantity;
+		
+        else if (item=="Salt") 
+			Price: 20*quantity;
+		
+        else if (item=="Toor Dal") 
+			Price: 90*quantity;
+		
+        else if (item=="Moong Dal") 
+			Price: 95*quantity;
+		
+        else if (item=="Tomato") 
+			Price: 25*quantity;
+		
+        else if (item=="Onion") 
+			Price: 30*quantity;
+		
+        else if (item=="Potato") 
+			Price: 22*quantity;
+		
+        else if (item=="Garlic") 
+			Price: 80*quantity;
+		
+        else if (item=="Ginger") 
+			Price: 60*quantity;
+		
+        else if (item=="Green Chilli") 
+			Price: 40*quantity;
+		
+        else if (item=="Apple") 
+			Price: 100*quantity;
+		
+        else if (item=="Banana") 
+			Price: 40*quantity;
+		
+        else if (item=="Mango") 
+			Price: 120*quantity;
+		
+        else if (item=="Orange") 
+			Price: 60*quantity;
+		
+        else if (item=="Grapes") 
+			Price: 70*quantity;
+		
+        else if (item=="Lemon") 
+			Price: 10*quantity;
+		
+        else if (item=="Coriander") 
+			Price: 10*quantity;
+		
+        else if (item=="Mint") 
+			Price: 10*quantity;
+		
+        else if (item=="Curd") 
+			Price: 40*quantity;
+		
+        else if (item=="Paneer") 
+			Price: 90*quantity;
+		
+        else if (item=="Ice Cream") 
+			Price: 100*quantity;
+		
+        else System.out.println("Item not available");
+		
+		return Price;
+	}
+    
 }
